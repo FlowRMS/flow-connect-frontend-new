fb4e6ee85a745df87436abe59dc6f2953a538da6
Subhan Farooq <ksubhan444@gmail.com>
2026-01-09 07:28:25 +0300

    Fix incorrect /checks/ URLs to use /commissions/ instead
    Updated check entity redirects to use correct /commissions/ route:
    - Fixed redirect URL in upload-complete page when processing check entities
    - Updated universal search check entity href to use /commissions/
    
    The /checks/ route does not exist; all check entities should use /commissions/.

--- components/orders/detail/components/tabs/LinkedObjectsTab.tsx
+++ components/orders/detail/components/tabs/LinkedObjectsTab.tsx
@@ -21,6 +21,7 @@ export function LinkedObjectsTab({ orderId }: LinkedObjectsTabProps) {
           sourceEntityType="ORDER"
           title="Linked Objects"
           showAddLinkButton={true}
+          enabledCategories={['contacts', 'companies', 'pre-opportunities', 'quotes', 'orders', 'invoices', 'checks', 'jobs', 'files']}
         />
       </div>
     </div>
