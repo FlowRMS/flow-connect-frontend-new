025b5003a8d7471a3b94431d5977f273d68d4963
Subhan Farooq <ksubhan444@gmail.com>
2026-02-03 23:23:02 +0300

    fixed contact page company dropdown reintroudced by sali

--- components/contacts/modals/CreateContactModal.tsx
+++ components/contacts/modals/CreateContactModal.tsx
@@ -392,22 +392,6 @@ export default function CreateContactModal({ isOpen, onClose, onSuccess }: Creat
                 </p>
               </div>
 
-              {/* Company */}
-              <div>
-                <label className={labelClass}>
-                  <svg className="w-4 h-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
-                  </svg>
-                  Company
-                </label>
-                <CompanySelect
-                  value={formData.companyId || ''}
-                  companies={companies || []}
-                  isLoading={companiesLoading}
-                  onChange={(id) => setFormData({ ...formData, companyId: id })}
-                />
-              </div>
-
               {/* Territory */}
               <div>
                 <label className={labelClass}>
